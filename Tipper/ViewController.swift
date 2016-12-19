9fcf98e28979f9f9f22cf9745925ca412ea93c26
whuang8 <huang554@purdue.edu>
2016-12-18 22:27:02 -0500

    Make bill field first responder and save bill amount after restart

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -14,15 +14,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    g
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let defaults = UserDefaults.standard
+        let billAmount = defaults.double(forKey: "billAmount")
+        var time = NSDate.init()
+        if (defaults.object(forKey: "time") != nil){
+            time = defaults.object(forKey: "time") as! NSDate
+            print(time.timeIntervalSinceNow)
+        }
+        
+        if(time.timeIntervalSinceNow <= 600 && billAmount != 0){
+            billField.text = String(format: "%.2f", billAmount)
+        }
         tipControl.selectedSegmentIndex = getDefaultIndex()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        billField.becomeFirstResponder()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,17 +38,25 @@ class ViewController: UIViewController {
         updateFields()
     }
     
-    func getDefaultIndex() -> Int {
-        let defaults = UserDefaults.standard
-        return defaults.integer(forKey: "selectedIndex")
-    }
     
-    @IBAction func onTap(_ sender: Any) {
+    @IBAction func onTap(_ sender: AnyObject) {
+        billField.resignFirstResponder()
         view.endEditing(true)
     }
 
     @IBAction func calculateTip(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        let billAmount = Double(billField.text!) ?? 0.0
+        let time = NSDate()
+        defaults.set(billAmount, forKey: "billAmount")
+        defaults.set(time, forKey: "time")
+        defaults.synchronize()
         updateFields()
+    }
+    
+    func getDefaultIndex() -> Int {
+        let defaults = UserDefaults.standard
+        return defaults.integer(forKey: "selectedIndex")
     }
     
     func updateFields() -> Void {
@@ -52,6 +67,11 @@ class ViewController: UIViewController {
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 
