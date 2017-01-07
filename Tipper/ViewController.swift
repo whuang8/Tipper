242f222d267942a0cc0bac9b0ef39e2b1852bc9c
whuang8 <huang554@purdue.edu>
2017-01-07 13:23:00 -0500

    Add colors and icons 🎉

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var onePersonLabel: UILabel!
+    @IBOutlet weak var twoPeopleLabel: UILabel!
+    @IBOutlet weak var threePeopleLabel: UILabel!
+    @IBOutlet weak var fourPeopleLabel: UILabel!
+    @IBOutlet weak var fivePeopleLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +38,22 @@ class ViewController: UIViewController {
         }
         tipControl.selectedSegmentIndex = getDefaultIndex()
         billField.becomeFirstResponder()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
+        
+        // Optionally initialize the property to a desired starting value
+        self.firstView.alpha = 0
+        self.secondView.alpha = 1
+        UIView.animate(withDuration:0.4, animations: {
+            // This causes first view to fade in and second view to fade out
+            self.firstView.alpha = 1
+            if (self.billField.text?.isEmpty)! {
+                self.secondView.alpha = 0
+            } else {
+                self.secondView.alpha = 1
+            }
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,6 +76,15 @@ class ViewController: UIViewController {
         defaults.set(time, forKey: "time")
         defaults.synchronize()
         updateFields()
+        
+        UIView.animate(withDuration:0.8, animations: {
+            if (self.billField.text?.isEmpty)! {
+                self.secondView.alpha = 0
+            } else {
+                self.secondView.alpha = 1
+
+            }
+        })
     }
     
     func getDefaultIndex() -> Int {
@@ -64,9 +97,18 @@ class ViewController: UIViewController {
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
+        let splitTwo = total / 2
+        let splitThree = total / 3
+        let splitFour = total / 4
+        let splitFive = total / 5
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        onePersonLabel.text = String(format: "$%.2f", total)
+        twoPeopleLabel.text = String(format: "$%.2f", splitTwo)
+        threePeopleLabel.text = String(format: "$%.2f", splitThree)
+        fourPeopleLabel.text = String(format: "$%.2f", splitFour)
+        fivePeopleLabel.text = String(format: "$%.2f", splitFive)
     }
     
     override func didReceiveMemoryWarning() {
